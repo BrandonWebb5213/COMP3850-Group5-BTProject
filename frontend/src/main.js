98770b98634f6f33a304cef42d9aa67b12d035cd
Elara Webb <52303452+BrandonWebb5213@users.noreply.github.com>
2022-09-23 03:48:41 +1000

    Further edits to prototype

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,21 +1,54 @@
 import * as views from './views.js';
 
+let superfundList = ["Fund 1", "Fund 2", "Fund 3", "Fund 4", "Fund 5", "Fund 6"];
+let concernList = ["Concern 1", "Concern 2", "Concern 3", "Concern 4", "Concern 5", "Concern 6"];
+
+let selectedSuperfunds = [0, 0, 0, 0, 0, 0];
+let selectedConcerns = [0, 0, 0, 0, 0, 0];
+
+let form = document.getElementById("sidebar");
+//form.addEventListener('submit', loadSelectedContent);
+
 function redraw() {
     let targetId = "main";
     if (window.location.hash == "#!/info") {
-        views.infoPage(targetId);
+        views.mainPage(targetId, superfundList, concernList, selectedSuperfunds, selectedConcerns);
+        views.infoPage();
     }
     else if (window.location.hash == "") {
-        views.mainPage(targetId);
+        views.mainPage(targetId, superfundList, concernList, selectedSuperfunds, selectedConcerns);
     }
     else {
         views.missingPage(targetId);
     }
 }
 
+function loadSelectedContent() {
+    for (let i = 0; i < superFundList.length; i++) {
+        let id = "fund" + i;
+        if (document.getElementById(id).value == 1) {
+            selectedSuperfunds[i] = 1;
+        }
+        else {
+            selectedSuperFunds[i] = 0;
+        }
+    }
+    for (let i = 0; i < concernList.length; i++) {
+        let id = "concern" + i;
+        if (document.getElementById(id).value == 1) {
+            selectedConcerns[i] = 1;
+        }
+        else {
+            selectedConcerns[i] = 0;
+        }
+    }
+    console.log(selectedSuperfunds);
+    console.log(selectedConcerns)
+}
+
 window.onhashchange = function () {
     redraw();
-}
+};
 window.onload = function() {
     redraw();
 };
--- frontend/src/views.js
+++ frontend/src/views.js
@@ -1,48 +1,82 @@
 export {infoPage, mainPage, missingPage};
 
-function infoPage(targetId) {
-    document.getElementById(targetId).innerHTML = "info";
+function infoPage() {
+    document.getElementById("content-area").innerHTML = "<div id=\"help-dialogue\" class=\"content-block\">" +
+    "There will be some information about what the tool does. This includes an explanation of ESGs."
+    "</div>";
+    document.getElementById("help-control").innerHTML = "<a href=\".\/\"" +
+        "class=\"help-icon\"" +
+        "id=\"help-close-icon\"" + 
+        "alt=\"close help menu\">?<div id=\"little-x\">x</div></a>"
 }
 
-function mainPage(targetId) {
-    document.getElementById(targetId).innerHTML = generateSidebar();
+function mainPage(targetId, superfundList, concernList, selectedSuperfunds, selectedConcerns) {
+    document.getElementById(targetId).innerHTML = generateSidebar(superfundList, concernList, selectedSuperfunds, selectedConcerns) +
+    "<article id=\"content-area\"><article>";
+    document.getElementById("help-control").innerHTML = "<a href=\"./#!/info\"" +
+        "class=\"help-icon\"" + 
+        "alt=\"help information\">?</a>"
 }
 
 function missingPage(targetId) {
     document.getElementById(targetId).innerHTML = "This page does not exist.";
 }
 
-const generateSidebar = () => {
-    return "<div id=\"sidebar\">" +
-        "<div id=\"super-fund-list\">" +
+const generateSidebar = (superfundList, concernList, selectedSuperfunds, selectedConcerns) => {
+    return "<form id=\"sidebar\">" +
+        "<div id=\"superfund-list\" class=\"sidebar-list\">" +
             "<p class=\"sidebar-category-heading\">Super Funds</p>" +
-            generateSuperList() +
+            generateSuperfundList(superfundList, selectedSuperfunds) +
         "</div>" +
-        "<div id=\"area-concern-list\">" +
+        "<div id=\"area-concern-list\" class=\"sidebar-list\">" +
             "<p class=\"sidebar-category-heading\">Areas of Concern</p>" +
-            generateConcernList() +
+            generateConcernList(concernList , selectedConcerns) +
         "</div>" +
-    "</div>";
+    "</form>";
 }
 
-const generateSuperList = () => {
-    return "<ul>" +
-        "<li>Fund 1</li>" +
-        "<li>Fund 2</li>" +
-        "<li>Fund 3</li>" +
-        "<li>Fund 4</li>" +
-        "<li>Fund 5</li>" +
-        "<li>Fund 6</li>" +
-    "</ul>"
+const generateSuperfundList = (superfundList, selectedSuperfunds) => {
+    let result = "";
+    let numFunds = 0;
+    for (const superfund of superfundList) {
+        let tag = "fund" + numFunds;
+        let selected = "";
+        if (selectedSuperfunds[numFunds] == 1) {
+            selected = "checked";
+        }
+        let input = "<input type=\"checkbox\" " +
+            "id=\"" + tag + "\" " +
+            "name=\"" + tag + "\" " +
+            "value=\"" + 1 + "\"" + 
+            "onchange=\"this.form.submit()\""+
+            selected + ">";
+
+        let label = "<label for=\"" + tag + "\">" + superfund + "</label>";
+        result += input + label + "<br>";
+        numFunds += 1;
+    }
+    return result;
 }
 
-const generateConcernList = () => {
-    return "<ul>" +
-        "<li>Concern 1</li>" +
-        "<li>Concern 2</li>" +
-        "<li>Concern 3</li>" +
-        "<li>Concern 4</li>" +
-        "<li>Concern 5</li>" +
-        "<li>Concern 6</li>" +
-    "</ul>"
+const generateConcernList = (concernList, selectedConcerns) => {
+    let result = "";
+    let numConcerns = 0;
+    for (const concern of concernList) {
+        let tag = "concern" + numConcerns;
+        let selected = "";
+        if (selectedConcerns[numConcerns] == 1) {
+            selected = "checked";
+        }
+        let input = "<input type=\"checkbox\" " +
+            "id=\"" + tag + "\" " +
+            "name=\"" + tag + "\" " +
+            "value=\"" + 1 + "\" " + 
+            "onchange=\"this.form.submit()\"" +
+            selected + ">";
+            
+        let label = "<label for=\"" + tag + "\">" + concern + "</label>";
+        result += input + label + "<br>";
+        numConcerns += 1;
+    }
+    return result;
 }
