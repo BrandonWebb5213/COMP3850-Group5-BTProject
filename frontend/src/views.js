e58d744520cea2f613344bd757b58884b786a2ef
Elara Webb <52303452+BrandonWebb5213@users.noreply.github.com>
2022-09-23 11:13:32 +1000

    Added comments to views.js

--- frontend/src/views.js
+++ frontend/src/views.js
@@ -1,16 +1,40 @@
-export {infoPage, mainPage, missingPage};
+export {infoPage, mainPage, missingPage}; // export functions for use by main.js
 
+/**
+ * Html code for the site when the info box is open.
+ * This is run after the mainPage function and will run in conjunction with it.
+ * It will open up the info box in content area of the site, as well as
+ * change the help icon in the header into a 'close help box' icon.
+ */
 function infoPage() {
+    // Inject the help dialogue box into the content area of the site
     document.getElementById("content-area").innerHTML = "<div id=\"help-dialogue\" class=\"content-block\">" +
     "<div class=\"content-block-heading\">Information on the ESG Comparison Tool</div><br>" +
     "There will be some information about what the tool does. This includes an explanation of ESGs and BT Financial."
     "</div>";
+
+    // Inject close help icon into site. 
+        // This will navigate back to the main page and close the help box.
     document.getElementById("help-control").innerHTML = "<a href=\".\/\"" +
         "class=\"help-icon\"" +
         "id=\"help-close-icon\"" + 
         "alt=\"close help menu\">?<div id=\"little-x\">x</div></a>"
 }
 
+/**
+ * Loads the content of the site into the main div.
+ * This includes loading the sidebar and the main content of the site.
+ * 
+ * @param {*} targetId The ID in the html document to inject the content into.
+ * @param {*} superfundList The array of super funds received from main.js. 
+ *  Used to generate the content of the site.
+ * @param {*} concernList The array of markets of concern received from main.js. 
+ *  Used to generate the content of the site.
+ * @param {*} selectedSuperfunds The array of selected super funds received from main.js.
+ *  Used to determine what super funds will appear in the content area.
+ * @param {*} selectedConcerns The array of selected markets of concern received from main.js.
+ *  Used to determing what markets of concern will appear in the content area.
+ */
 function mainPage(targetId, superfundList, concernList, selectedSuperfunds, selectedConcerns) {
     document.getElementById(targetId).innerHTML = generateSidebar(superfundList, concernList, selectedSuperfunds, selectedConcerns) +
     "<article id=\"content-area\"><article>";
@@ -19,10 +43,30 @@ function mainPage(targetId, superfundList, concernList, selectedSuperfunds, sele
         "alt=\"help information\">?</a>"
 }
 
+/**
+ * Loads an error code if the hash code is wrong.
+ * 
+ * @param {*} targetId The ID in the html document to inject the content into.
+ */
 function missingPage(targetId) {
     document.getElementById(targetId).innerHTML = "This page does not exist.";
 }
 
+/**
+ * Generate the html code for the sidebar of the site.
+ * The various categories of super funds are loaded as a form by the generateSuperfundList 
+ * and generateConcernList functions.
+ * 
+ * @param {*} superfundList The array of super funds received from main.js. 
+ *  Used to generate the content of the site.
+ * @param {*} concernList The array of markets of concern received from main.js. 
+ *  Used to generate the content of the site.
+ * @param {*} selectedSuperfunds The array of selected super funds received from main.js.
+ *  Used to determine what super funds will appear in the content area.
+ * @param {*} selectedConcerns The array of selected markets of concern received from main.js.
+ *  Used to determing what markets of concern will appear in the content area.
+ * @returns The content of the sidebar to use for injection by other functions.
+ */
 const generateSidebar = (superfundList, concernList, selectedSuperfunds, selectedConcerns) => {
     return "<form id=\"sidebar\" onSubmit=\"loadSelectedContent()\">" +
         "<div id=\"superfund-list\" class=\"sidebar-list\">" +
@@ -36,6 +80,15 @@ const generateSidebar = (superfundList, concernList, selectedSuperfunds, selecte
     "</form>";
 }
 
+/**
+ * Generate a list of checkboxes representing super funds for the sidebar.
+ * 
+ * @param {*} superfundList The array of super funds received from main.js. 
+ *  Used to generate the content of the site.
+ * @param {*} selectedSuperfunds The array of selected super funds received from main.js.
+ *  Used to determine what super funds will appear in the content area.
+ * @returns A list of checkboxes representing the various super funds.
+ */
 const generateSuperfundList = (superfundList, selectedSuperfunds) => {
     let result = "";
     let numFunds = 0;
@@ -59,6 +112,15 @@ const generateSuperfundList = (superfundList, selectedSuperfunds) => {
     return result;
 }
 
+/**
+ * Generate a list of checkboxes representing areas of concern for the sidebar.
+ * 
+ * @param {*} concernList The array of areas of concern received from main.js. 
+ *  Used to generate the content of the site.
+ * @param {*} selectedConcerns The array of selected markets of concern received from main.js.
+ *  Used to determine what markets of concern will appear in the content area.
+ * @returns A list of checkboxes representing the various markets of concern.
+ */
 const generateConcernList = (concernList, selectedConcerns) => {
     let result = "";
     let numConcerns = 0;
